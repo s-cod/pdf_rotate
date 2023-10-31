ae4c94fda25fbab29d927f77dd0f004b94e49512
s-cod <89507850000s@gmail.com>
2023-10-31 16:45:00 +0600

    renaming variables

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 
 def pdf_rotate(name: str):
     pdf = PdfReader(f"./in/{name}")
-    pdfw = PdfWriter()
-    for i in pdf.pages:
-        i.rotate(90)
-        pdfw.add_page(i)
+    pdf_w = PdfWriter()
+    for p in pdf.pages:
+        p.rotate(90)
+        pdf_w.add_page(p)
     with open(f"./out/{name}", "wb") as f:
-        pdfw.write(f)
+        pdf_w.write(f)
 
 def main():
     files = os.listdir("./in")
